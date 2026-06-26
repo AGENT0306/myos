4b1d6c638834d67c8fe0679da7ea2e439353e16b
Rei Trebicka <reitrebicka74@gmail.com>
2026-06-26 13:56:29 -0400

    added vmm files as well as update obj files

---
+++ kernel/src/vmm.h
@@ -0,0 +1,19 @@
+//
+// Created by Rei Trebicka on 6/26/2026.
+//
+
+#ifndef MYOS_VMM_H
+#define MYOS_VMM_H
+
+#include <stdint.h>
+
+typedef struct addr_space
+{
+    uint64_t pml4;
+}addr_space_t;
+
+void vmm_init(uint64_t hhdm_offset);
+void map_page(uint64_t frame, addr_space_t pml4, uint64_t vaddr, uint64_t flags);
+void unmap_page( addr_space_t pml4, uint64_t vaddr);
+
+#endif //MYOS_VMM_H
